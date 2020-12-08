5cdb8e70bac21f2cfa1b50ab87a1df41dbf41cac
aliselmes <a.selmes@gmail.com>
2020-12-07 20:31:59 -0600

    updated jumbotrons on home page

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,114 +1,69 @@
 import React from 'react';
-import { Jumbotron, Button, Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle } from 'reactstrap';
-
+import { Jumbotron, Container, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 const HomeJumbotron = (props) => {
     return (
       <div>
-        <Jumbotron id="jumbotron">
-            <div id="jumbotron-container">
-                <h1 className="display-3">Buy. Sell. Trade.</h1>
-                <p className="lead">With The Music Exchange, YOU are in control.</p>
-                <hr className="my-2" />
-                <p>Shop thousands of used instruments, pro audio equipment, parts &amp; more. </p>
-                <p>Easily post a listing and start selling your items today! </p>
-                <p>View trade-in options and negotiate with the seller. </p>
-                <p className="lead">
-                <Button color="primary">Learn More</Button>
-                </p>
-            </div>
-        </Jumbotron>
+        <div>
+          <Jumbotron fluid id="buyselltrade" className="my-4">
+            <Container fluid>
+              <h1 className="display-3">Buy. Sell. Trade. <strong>LOCALLY.</strong></h1>
+              <p className="lead">Have gear you need to get off your hands? List an item for sale! Or, find musicians in your area with used gear to sell or trade.</p>
+              <Link className="nav-link" to="/directory">
+                <Button>Browse</Button> 
+              </Link>
+            </Container>
+          </Jumbotron>
+        </div>
+        <hr />
+        <div>
+          <Jumbotron fluid id="musiciansseeking" className="my-4">
+            <Container fluid>
+              <h1 className="display-3">Find Musicians Near You</h1>
+              <p className="lead">Seeking musicians? Looking to join a band? Find like-minded people near you today.</p>
+              <Link className="nav-link" to="/">
+                <Button>Search</Button> 
+              </Link>
+            </Container>
+          </Jumbotron>
+        </div>
+        <hr />
+        <div>
+          <Jumbotron fluid id="gigs" className="my-4">
+            <Container fluid>
+              <h1 className="display-3">Play Local Venues</h1>
+              <p className="lead">Looking for a gig? Local venues use US to search for musicians to hire!</p>
+              <Link className="nav-link" to="/">
+                <Button>Gigs</Button> 
+              </Link>
+            </Container>
+          </Jumbotron>
+        </div>
+        <hr />
+        <div>
+          <Jumbotron fluid id="learn" className="my-4">
+            <Container fluid>
+              <h1 className="display-3">Learn With a Local Instructor</h1>
+              <p className="lead">Find an instructor near you and begin your musical journey!</p>
+              <Link className="nav-link" to="/">
+                <Button>Instructors</Button> 
+              </Link>
+            </Container>
+          </Jumbotron>
+        </div>
+        <hr />
+
+
+
       </div>
     );
   };
 
-const HomeCards = (props) => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 col-md-4">
-          <Card>
-            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-        </div>
-
-        <div className="col-12 col-md-4">
-          <Card>
-            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-        </div>
-
-        <div className="col-12 col-md-4">
-          <Card>
-            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-12 col-md-4">
-          <Card>
-            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-        </div>
-
-        <div className="col-12 col-md-4">
-          <Card>
-            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-        </div>
-
-        <div className="col-12 col-md-4">
-          <Card>
-            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-            <CardBody>
-              <CardTitle tag="h5">Card title</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-        </div>
-      </div>
-    </div>
-  );
-}
   
 function Home(props) {
     return (
         <div className="container">
             <HomeJumbotron />
-            <HomeCards />
         </div>
     );
 }
